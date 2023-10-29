9372224e736ab37d5b9dd0cba25dae3819da64f6
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-10-29 18:44:20 +0800

    Update test.js

--- test.js
+++ test.js
@@ -24,31 +24,14 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     
 
-	const options = {
- 	 method: 'POST',
-  	hostname: 'api.render.com',
-  	port: null,
-  	path: '/v1/services/srv-ckud7cmb0mos738u2ssg/suspend',
-  	headers: {
-   	 accept: 'application/json',
-    	authorization: 'Bearer rnd_dbjVtRsFHMVGqUPbdHtlPLN4ulbq'
-  	}
-	};
+	const sdk = require('api')('@render-api/v1.0#1ifry11lo91zxhg');
 
-	const req = http.request(options, function (res) {
- 	 	const chunks = [];
+	sdk.auth('rnd_dbjVtRsFHMVGqUPbdHtlPLN4ulbq');
+	sdk.resumeService({serviceId: 'srv-ckud7cmb0mos738u2ssg'})
+  	.then(({ data }) => console.log(data))
+  	.catch(err => console.error(err));
+		});
 
-  		res.on('data', function (chunk) {
-   		 chunks.push(chunk);
-  		});
-
-  		res.on('end', function () {
-   		 const body = Buffer.concat(chunks);
-    	 console.log(body.toString());
-  		});
-	});
-
-	req.end();
 });
 
 const PORT = process.env.PORT || 5500;
